b6e5788503024e1d0763438f7a07647130641e84
wistonmosqueradev <wistonmosqueradev@gmail.com>
2026-07-01 09:50:54 -0500

    add wiston

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation wiston 333
           </a>
         </div>
       </main>
